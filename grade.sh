33b9dafb60ce3b7f5b2c8524b915b8cb5d9654ed
lplums <122485613+lplums@users.noreply.github.com>
2023-03-13 21:28:07 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,7 +5,8 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 cd student-submission
-if [[ -f ListExamples.java]]
+
+if [[ -f 'ListExamples.java' ]]
 then 
   echo "ListExamples found"
 else
