cf571959ca2eb331076de6bff6c9d104c2480eed
lplums <122485613+lplums@users.noreply.github.com>
2023-03-13 22:54:44 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,6 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission
-git clone $1 student-submission
+git clone $1
 echo 'Finished cloning'
 
 if [[ -f 'ListExamples.java' ]]
