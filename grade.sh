3bda3a9a7a843befbc785fcaf65bf0304581bad4
lplums <122485613+lplums@users.noreply.github.com>
2023-03-13 17:54:43 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,26 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+if [[ -f ListExamples.java]]
+then 
+  echo "ListExamples found"
+else
+  echo "ListExamples not found"
+  exit 1
+fi
+
+javac -cp $CPATH *.java > compile_output.txt
+if [[ grep -c "error" compile_output.txt = 0]]
+then 
+  echo "Compile success"
+else 
+  echo "Compile failed"
+  exit 2
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > execute_output.txt
+if [[ grep -c "FAILURES" execute_output.txt = 0]]
+then
+  echo "
